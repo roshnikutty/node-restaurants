9af6e9c98d9ed72d447a0b24443cce4465dfeaad
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-05-09 21:45:57 -0400

    test

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(bodyParser.json());
 
 
-// GET requests to /restaurants => return 10 restaurants
+// GET requests to /restaurants => return 10 restaurants here
 app.get('/restaurants', (req, res) => {
   Restaurant
     .find()
